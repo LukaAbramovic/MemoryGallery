22a08abde998e2f0dcb88c41d58777ff553a18b0
Mia Rovis <mia.rovis@gmail.com>
2021-01-07 12:31:32 +0100

    commit on master

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -18,11 +18,11 @@
  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
  </div>
  </br>
- <button type="login" class="btn btn-primary" id="button1">Login</button>
+ <button type="button" @click="login()" class="btn btn-primary" id="button1">Login</button>
  </br></br>
- <button type="Facebook" class="btn btn-primary" id="button1"><img src="@/assets/facebook.png" height="30">Login with Facebook</button>
+ <button type="Facebook" class="btn btn-primary" id="button2"><img src="@/assets/facebook.png" height="30">Login with Facebook</button>
   </br></br>
- <button type="Google" class="btn btn-primary" id="button1"><img src="@/assets/google.png" height="30">Login with Google</button>
+ <button type="Google" class="btn btn-primary" id="button3"><img src="@/assets/google.png" height="30">Login with Google</button>
  </form>
  </div>
  <div class="col-sm"></div>
@@ -30,8 +30,38 @@
  </div>
  </div>
 </template>
+import {Firebase} from "@/Firebase";
 
 <style lang="scss">
 #button1 {
 background-color:#5F5547;}
+#button2 {
+background-color:#5F5547;}
+#button3 {
+background-color:#5F5547;}
 </style>
+
+<script>
+export default {
+    name: "login",
+    data() {
+           return{
+           username: '',
+           password: '',
+           };
+    },
+    methods: {
+        login() {
+        console.log("login..." + this.username);
+        Firebase.auth().signInWithEmailAndPassword(this.username, this.password)
+            .then((result) => {
+                console.log("Uspjesna prijava", result);
+                this. $router.replace({name: "home" });
+            })
+           .catch(function(e){
+                console.error("greska", e);
+        });
+        },
+    },
+};
+</script>

7262e66f0569c585bc0b3ed4fec683bf65bfec35
Team-4-prog <memory.gallery7@gmail.com>
2021-01-18 11:45:42 +0100

    commit on master

--- src/components/Slike.vue
+++ src/components/Slike.vue
@@ -1,19 +1,23 @@
 <template>
-<div class="card text-center">
-<div class="card-header">
+ <div class="card">
+ <div class="card-header text-left">
  Featured
-</div>
-<div class="card-body p-0">
-<img class="card-img-top" src="google,png"/>
-</div>
-<div class="card footer text-muted">
-5h ago...
-</div>
-</div>
+ </div>
+ <div class="card-body p-0">
+ <img class="card-img-top" src="/kamera.png" alt="Čovjek
+sa zlatnim zubima"/>
+ </div>
+ <div class="card-footer text-muted text-left">
+ 2 days ago
+ </div>
+ </div>
 </template>
 
 <script>
-export default{
-name: "Slike",
-};
+export default {
+ name: 'Slike',
+ props: {
+ msg: String
+ }
+}
 </script>
--- src/views/Trending.vue
+++ src/views/Trending.vue
@@ -1,22 +1,28 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-1">
-      
-      <slike/>
-    </div>
-    <div class="col-2">
-      
-    </div>
-  </div>
+ <div class="row">
+ <div class="col-1"></div>
+ <div class="col-7">
+ <Slike />
+ </div>
+ <div class="col-3">
+ Ovdje ide sidebar
+ </div>
+ </div>
 </template>
+
 <script>
-import Slike from "@/components/Slike.vue";
-  export default {
-         name: "trending",
-         components: {
-         Slike,
-         },
-  };
-  </script>
+import Slike from '@/components/Slike.vue'
+export default {
+ name: 'trending',
+ components: {
+ Slike
+ }
+}
+
+</script>
+<style lang="scss">
+ .card {
+ margin-bottom: 20px;
+ }
+</style>
   

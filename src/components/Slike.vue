33b7e6d28c41d704aefc9dc59cc9184a11b24e83
Team-4-prog <memory.gallery7@gmail.com>
2021-01-18 11:29:10 +0100

    commit on master

---
+++ src/components/Slike.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="card text-center">
+<div class="card-header">
+ Featured
+</div>
+<div class="card-body p-0">
+<img class="card-img-top" src="google,png"/>
+</div>
+<div class="card footer text-muted">
+5h ago...
+</div>
+</div>
+</template>
+
+<script>
+export default{
+name: "Slike",
+};
+</script>

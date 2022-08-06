a915e4cc7b3a03a5fb4a521864c258b8d9b92326
Mia Rovis <mia.rovis@gmail.com>
2022-08-06 17:44:34 +0200

    Update SignUp.vue

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,6 @@
 <template> 
  <div class="about">
- </br>
+
  <div class="container">
  <div class="row">
  <div class="col-sm"></div>
@@ -9,27 +9,27 @@
  <div class="form-group">
  <label for="exampleInputUsername1">User name</label>
  <input type="username" v-model="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username" />
- </br>
+ 
  </div>
  <div class="form-group">
  <label for="exampleInputname1">Name</label>
  <input type="name" v-model="name" class="form-control" id="exampleInputPassword1" placeholder="Name" />
  </div>
- </br>
+ 
  <div class="form-group">
  <label for="exampleInputEmail1">Email</label>
  <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter e-mail" />
- </br>
+ 
  </div>-
  <div class="form-group">
  <label for="exampleInputPassword1">Password</label>
  <input type="password" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
  </div>
- </br>
+ 
  <button type="button" @click="signup" class="btn btn-primary" id="button1">Submit</button>
- </br></br>
+ 
  <button type="Facebook" class="btn btn-primary" id="button1"><img src="@/assets/facebook.png" height="30">Sign up with Facebook</button>
- </br></br>
+ 
  <button type="Google" class="btn btn-primary" id="button1"><img src="@/assets/google.png" height="30">Sign up with Google</button>
  </form>
  </div>
